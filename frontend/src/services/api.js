e2a6eac45c716f2662a3c8f186aa2e0c27cfb37f
anurag <anuragsingh98352@gmail.com>
2025-12-05 15:24:50 +0530

    backend setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+console.log("🔥 VITE_BACKEND_URL =", import.meta.env.VITE_BACKEND_URL);
+
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
   timeout: 15000,
